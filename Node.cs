f38cbc074ff6b480b7df34c004bcdceefd5d77e8
Harsh <harsh.21483@gmail.com>
2021-06-15 23:46:48 -0700

    [Harsh]add:-New class for stack and queue is added

---
+++ Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack_Queue
+{
+    class Node
+    {
+        public int data;
+        public Node next;        //Key and next which is Associated to another node key So Kind of Node Iteration..
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Stack_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListStack list = new LinkedListStack();
+            list.push(70);
+            list.push(30);
+            list.push(56);
+            list.display();
+           
+
+
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            
+        }
+    }
+}
